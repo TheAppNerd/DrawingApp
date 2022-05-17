daf9d2548d592c41ac678a5d379f6f38e64a7ffd
Alex Thompson <Alex@appNerd.com.au>
2022-05-17 11:40:31 +1000

    implement save to phone functionality

--- DrawingApp/HomeVC.swift
+++ DrawingApp/HomeVC.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import PencilKit
+import PhotosUI
 
 class HomeVC: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
     
-    var drawingView = PKCanvasView()
+    var canvasView = PKCanvasView()
     let drawing = PKDrawing()
     let toolPicker = PKToolPicker()
+    let canvasWidth: CGFloat = 768
+    let canvasoverScrollHeight: CGFloat = 500
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,46 +23,95 @@ class HomeVC: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
         configurebarButtons()
         layoutUI()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let canvasScale = canvasView.bounds.width / canvasWidth
+        canvasView.minimumZoomScale = canvasScale
+        canvasView.maximumZoomScale = canvasScale
+        canvasView.zoomScale = canvasScale
+        
+        canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
+        updateContentSizeForDrawing()
+        
+    }
+    
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        updateContentSizeForDrawing()
+    }
 
     private func configure() {
-        view.addSubview(drawingView)
-        drawingView.translatesAutoresizingMaskIntoConstraints = false
-        drawingView.delegate = self
-        drawingView.drawing = drawing
-        drawingView.alwaysBounceVertical = true
-        drawingView.drawingPolicy = .anyInput
+        view.addSubview(canvasView)
+        canvasView.translatesAutoresizingMaskIntoConstraints = false
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = .anyInput
         
         
        
-        toolPicker.addObserver(drawingView)
-        toolPicker.setVisible(true, forFirstResponder: drawingView)
-        drawingView.becomeFirstResponder()
+        toolPicker.addObserver(canvasView)
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        canvasView.becomeFirstResponder()
     
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
     }
     
     private func configurebarButtons() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "camera"), style: .done, target: self, action: #selector(screenshotPressed))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Pencil", style: .done, target: self, action: #selector(pencilPressed))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Pencil", style: .done, target: self, action: #selector(toggleFingerOrPencil))
     }
     
     private func layoutUI() {
         NSLayoutConstraint.activate([
-            drawingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            drawingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            drawingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            drawingView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            canvasView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            canvasView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            canvasView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            canvasView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 
     
     @objc func screenshotPressed() {
+        UIGraphicsBeginImageContext(canvasView.bounds.size)
+        canvasView.drawHierarchy(in: canvasView.bounds, afterScreenUpdates: true)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        if image != nil {
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.creationRequestForAsset(from: image!)
+            }, completionHandler: { success, error in
+                
+                
+            })
+        }
+        
         
     }
     
-    @objc func pencilPressed() {
-        
+    @objc func toggleFingerOrPencil(sender: UIBarButtonItem) {
+        //add a bool here to switch between finger allowed and not.
+//        canvasView.drawingPolicy = .
+//        sender.title = canvasView.allowsFingerDrawing
     }
     
+    
+    func updateContentSizeForDrawing() {
+        let drawing = canvasView.drawing
+        let contentHeight: CGFloat
+        
+        if !drawing.bounds.isNull {
+            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvasoverScrollHeight) * canvasView.zoomScale)
+        } else {
+            contentHeight = canvasView.bounds.height
+        }
+        canvasView.contentSize = CGSize(width: canvasWidth * canvasView.zoomScale, height: contentHeight)
+    }
   
 }
 
